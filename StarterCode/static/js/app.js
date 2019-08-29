a846c577b0811eead37835e80f492abf711c79e9
divya tv <divya.tv@gmail.com>
2019-08-29 11:37:00 -0400

    creating new repo o start HW

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,25 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+// Select the button
+for (var i = 0; i < tableData.length; i++) {
+  console.log( `divya:tableData[i]`);
+
+  }
+  
+//   // Find the average score
+//   var avg = sum / movieScores.length;
+  
+//   // Store the length of movie ratings
+//   var numGoodMovies = goodMovieScores.length;
+//   var numOkMovies = okMovieScores.length;
+//   var numBadMovies = badMovieScores.length;
+  
+//   // Print results
+//   console.log("---------");
+//   console.log(`There are ${numGoodMovies} good movies.`);
+//   console.log(`There are ${numOkMovies} ok movies.`);
+//   console.log(`There are ${numBadMovies} bad movies.`);
+//   console.log(`The average movie rating is ${avg}.`);
+//   console.log("---------");
