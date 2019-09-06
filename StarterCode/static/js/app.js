3906286dd833a8f7b965127277b8de2e3197cd05
divya tv <divya.tv@gmail.com>
2019-09-06 12:59:52 -0400

    all the latest files

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,25 +1,42 @@
 // from data.js
-var tableData = data;
+var tabledata = data;
 
 // YOUR CODE HERE!
 // Select the button
-for (var i = 0; i < tableData.length; i++) {
-  console.log( `divya:tableData[i]`);
+var columns=[]
+columns=Object.keys(tabledata[0]);
+console.log(columns)
 
-  }
+var body=d3.select('tbody');
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+
+//filter-btn
+// Select the input element and get the raw HTML node
+var inputElement = d3.select("#datetime");
+ // Get the value property of the input element
+var inputValue = inputElement.property("value");
+
+//function filter(date){
+for(i=0; i< tabledata.length; i++){
+  //create a tr tag
+  body.append("tr");
+  for (j=0; j< columns.length ;j++){
+   //fill the td tags
+  //if (tabledata[i][columns['city']] == inputValue){
+ // d3.select("tbody").append("td").html(tabledata[i][columns[j]]);
+  //}
+    // print each item -console.log(tabledata[i][columns[j]]);
+  } 
+  console.log(tabledata[i][columns['city']])
+
+}
+}
+
+//console.log(filter);
+
+
+
+
   
-//   // Find the average score
-//   var avg = sum / movieScores.length;
-  
-//   // Store the length of movie ratings
-//   var numGoodMovies = goodMovieScores.length;
-//   var numOkMovies = okMovieScores.length;
-//   var numBadMovies = badMovieScores.length;
-  
-//   // Print results
-//   console.log("---------");
-//   console.log(`There are ${numGoodMovies} good movies.`);
-//   console.log(`There are ${numOkMovies} ok movies.`);
-//   console.log(`There are ${numBadMovies} bad movies.`);
-//   console.log(`The average movie rating is ${avg}.`);
-//   console.log("---------");
